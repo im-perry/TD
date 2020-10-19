c0a8feaaa9374485039dc95a11086ac9e9d71603
im-perry <61919070+im-perry@users.noreply.github.com>
2020-10-19 10:01:00 +0300

    Add files via upload

---
+++ symbols.h
@@ -0,0 +1,18 @@
+#ifndef __SYMBOLS_H__
+#define __SYMBOLS_H__
+#define END 0
+#define INT 1
+#define VOID 2
+#define WHILE 3
+#define IF 4
+#define ELSE 5
+#define RETURN 6
+#define CONSTANT 7
+#define ASSIGN 8
+#define ADD 9
+#define SUBSTRACT 10
+#define IDENTIFIER 11
+#define END_OF_INSTRUCTION 12
+#define ID 13
+#define NUM 14
+#endif
